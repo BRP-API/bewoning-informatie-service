5c55ee411ed2aa2ec82a567e522a4cf8c42eb5d4
Melvin Lee <melvin.lee@iswish.nl>
2024-11-28 11:25:20 +0100

    refactor: gebruik GeboorteBasisProfile van Brp.Shared.DtoMappers

--- src/Bewoning.Informatie.Service/DataTransferObjects.cs
+++ src/Bewoning.Informatie.Service/DataTransferObjects.cs
@@ -109,27 +109,6 @@ namespace Bewoning.Informatie.Service.Generated
     }
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
-    public partial class GeboorteBasis
-    {
-        /// <summary>
-        /// Datum waarop de persoon is geboren.
-        /// <br/>
-        /// </summary>
-        [Newtonsoft.Json.JsonProperty("datum", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-        public AbstractDatum Datum { get; set; }
-
-        private System.Collections.Generic.IDictionary<string, object> _additionalProperties;
-
-        [Newtonsoft.Json.JsonExtensionData]
-        public System.Collections.Generic.IDictionary<string, object> AdditionalProperties
-        {
-            get { return _additionalProperties ?? (_additionalProperties = new System.Collections.Generic.Dictionary<string, object>()); }
-            set { _additionalProperties = value; }
-        }
-
-    }
-
-    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
     public partial class Bewoner
     {
         [Newtonsoft.Json.JsonProperty("burgerservicenummer", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
