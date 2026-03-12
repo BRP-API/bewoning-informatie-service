c489fe1033506e336c1a927c2191389e1a6a581c
Melvin Lee <melvin.lee@iswish.nl>
2026-03-12 14:28:09 +0100

    chore: fix specs-verify script voor uitvoeren stap definitie docs

--- scripts/generate-step-summary.js
+++ scripts/generate-step-summary.js
@@ -7,6 +7,7 @@ fs.writeFileSync(outputFile, '', 'utf8');
 
 const fileMap = new Map([
     ["./../test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt", "docs (zonder integratie)"],
+    ["./../test-reports/cucumber-js/step-definitions/test-result-integratie-summary.txt", "docs (integratie)"],
     ["./../test-reports/cucumber-js/bewoning/test-result-geboortedatum-summary.txt", "geboortedatum"],
     ["./../test-reports/cucumber-js/bewoning/test-result-naam-summary.txt", "naam"],
     ["./../test-reports/cucumber-js/bewoning/test-result-raadpleeg-bewoning-met-periode-summary.txt", "raadpleeg bewoning met periode"],
